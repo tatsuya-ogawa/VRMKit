445e5f6e31d89780ffc754c6d1c5460b08d73191
tatsuya ogawa <togawa83@gmail.com>
2026-01-31 23:05:53 +0900

    revert Sources/VRMKit/VRM/Node.swift

--- Sources/VRMKit/VRM/Node.swift
+++ Sources/VRMKit/VRM/Node.swift
@@ -72,9 +72,15 @@ extension GLTF {
                         public let extras: CodableAny?
 
                         public enum RollAxis: String, Codable {
-                            case x = "X"
-                            case y = "Y"
-                            case z = "Z"
+                            case x
+                            case y
+                            case z
+
+                            private enum CodingKeys: String, CodingKey {
+                                case x = "X"
+                                case y = "Y"
+                                case z = "Z"
+                            }
                         }
                     }
 
@@ -86,12 +92,21 @@ extension GLTF {
                         public let extras: CodableAny?
 
                         public enum AimAxis: String, Codable {
-                            case positiveX = "PositiveX"
-                            case negativeX = "NegativeX"
-                            case positiveY = "PositiveY"
-                            case negativeY = "NegativeY"
-                            case positiveZ = "PositiveZ"
-                            case negativeZ = "NegativeZ"
+                            case positiveX
+                            case negativeX
+                            case positiveY
+                            case negativeY
+                            case positiveZ
+                            case negativeZ
+
+                            private enum CodingKeys: String, CodingKey {
+                                case positiveX = "PositiveX"
+                                case negativeX = "NegativeX"
+                                case positiveY = "PositiveY"
+                                case negativeY = "NegativeY"
+                                case positiveZ = "PositiveZ"
+                                case negativeZ = "NegativeZ"
+                            }
                         }
                     }
 
